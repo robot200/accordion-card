2ddfc72b601340058305a6a84b23697584acd825
robot200 <35541499+robot200@users.noreply.github.com>
2021-03-16 22:57:56 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-const dropdown = document.querySelector('.dropdown');
-const answer = document.querySelectorAll('.answer');
-
-dropdown.addEventListener('click', function(){
-    toggleHidden();
-})
-
-function toggleHidden() {
-    answer.classList.toggle('hidden');
-}
-
-// answer.forEach(function (a) {
-//     answer.addEventListener('click', function(e) {
-//         toggleHidden();
-//     })
-// })
+document.querySelectorAll('.question').forEach((question) => question.addEventListener('click', () => {
+    if(question.parentNode.classList.contains('active')) {
+        question.parentNode.classList.toggle('active')
+    } else {
+        document.querySelectorAll('.question').forEach(question => question.parentNode.classList.remove('active'))
+        question.parentNode.classList.add('active')
+    }
+}))
